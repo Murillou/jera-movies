b6f0cd313a8f69ad7015ff465e721a517da509a2
Murillou <Murillovinicius13@hotmail.com>
2024-05-21 16:43:06 -0300

    [feat] repositório inicializado

---
+++ front-end/src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="flex justify-center max-w-7xl mx-auto p-5 font-bold text-4xl">
+      <h1>Jera Movies</h1>
+    </header>
+  );
+}
---
+++ front-end/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'base-bg': '#1C1A1A',
+      },
+    },
+  },
+  plugins: [],
+};
